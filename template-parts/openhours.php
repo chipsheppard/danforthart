c2e724b949c806ba0a542139f8820084ae29ead4
Chip Sheppard <chip@sheppco.com>
2019-02-19 19:55:01 -0500

    initial commit

--- template-parts/openhours.php
+++ template-parts/openhours.php
@@ -18,7 +18,11 @@ if ( ! defined( 'ABSPATH' ) ) {
  -----------------------------------------------------------------
  */
 
-$day = date( 'l' );
+$date = date_create();
+date_sub( $date, date_interval_create_from_date_string( '5 hours' ) );
+$day = date_format( $date, 'l' );
+
+// $day = date( 'l' );
 $day_closed = null;
 $opens = null;
 $closes = null;
@@ -29,6 +33,8 @@ if ( have_rows( 'day', 'option' ) ) :
 	while ( have_rows( 'day', 'option' ) ) :
 		the_row();
 
+		$weekday = get_sub_field( 'day_of_week', 'option' );
+
 		if ( ! get_sub_field( 'open', 'option' ) ) :
 			$day_closed = get_sub_field( 'day_of_week', 'option' );
 		endif;
@@ -37,7 +43,6 @@ if ( have_rows( 'day', 'option' ) ) :
 			$opens = get_sub_field( 'open', 'option' );
 			$closes = get_sub_field( 'close', 'option' );
 		endif;
-
 	endwhile;
 endif;
 
@@ -54,23 +59,57 @@ endif;
 
 // HOURS LISTING ===================.
 if ( have_rows( 'day', 'option' ) ) :
+	$isday = 0;
+	$isopen = 0;
+	$isclosed = 0;
+	$firstday = 0;
+	$c = 0;
 	while ( have_rows( 'day', 'option' ) ) :
 		the_row();
 
 		echo '<div class="hours-row">';
-		echo '<div class="col-1-2 first">';
-		the_sub_field( 'day_of_week', 'option' );
-		echo '</div>';
-		echo '<div class="col-1-2">';
-		if ( get_sub_field( 'open', 'option' ) ) :
-			the_sub_field( 'open', 'option' );
-			echo ' - ';
-			the_sub_field( 'close', 'option' );
-			echo '</div>';
+
+		$myday = get_sub_field( 'day_of_week', 'option' );
+		$myopen = get_sub_field( 'open', 'option' );
+		$myclose = get_sub_field( 'close', 'option' );
+
+		if ( 0 === $isopen || 0 === $isclosed ) :
+			$isday = $myday;
+			$isopen = $myopen;
+			$isclosed = $myclose;
 		else :
-			echo 'Closed</div>';
+			if ( $myopen === $isopen && $myclose === $isclosed ) :
+				if ( 0 === $c ) :
+					$firstday = $isday;
+				endif;
+				$c++;
+				$isday = $myday;
+				$isopen = $myopen;
+				$isclosed = $myclose;
+			else :
+				$c = 0;
+				echo '<div class="col-1-2 first">';
+				if ( 0 !== $firstday ) :
+					echo esc_html( $firstday ) . ' - ';
+					$firstday = 0;
+				endif;
+				echo esc_html( $isday );
+				echo '</div>';
+				echo '<div class="col-1-2">';
+				if ( $isopen ) :
+					echo esc_html( $isopen );
+					echo ' - ';
+					echo esc_html( $isclosed );
+					echo '</div>';
+				else :
+					echo 'Closed</div>';
+				endif;
+				$isday = $myday;
+				$isopen = $myopen;
+				$isclosed = $myclose;
+			endif;
 		endif;
-		echo '</div>';
+		echo '</div>'; // hours-row.
 	endwhile;
 endif;
-echo '</div>';
+echo '</div>'; // open-hours.
